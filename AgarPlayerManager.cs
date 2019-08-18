f3b8114f07a3f7a2d94d9aab459e3b6836bb4315
Zachary Higgins <zachhiggins@gmail.com>
2019-08-17 21:44:56 -0400

    Completed initial setup of player spawn.
    Console output now showing on server exe when client launched.

--- AgarPlayerManager.cs
+++ AgarPlayerManager.cs
@@ -1,12 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DarkRift.Server;
+using DarkRift;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace PlanetGorgon_Server
 {
-    public class AgarPlugin
+
+    public class AgarPlayerManager : Plugin
     {
+
+        const float MAP_WIDTH = 20;
+
+        public override bool ThreadSafe => false;
+
+        public override Version Version => new Version(1, 0, 0);
+
+        Dictionary<IClient, Player> players = new Dictionary<IClient, Player>();
+
+        public AgarPlayerManager(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+            ClientManager.ClientConnected += ClientConnected;
+        }
+
+        private void ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            Random r = new Random();
+            Player newPlayer = new Player(
+                e.Client.ID,
+                (float)r.NextDouble() * MAP_WIDTH - MAP_WIDTH / 2,
+                (float)r.NextDouble() * MAP_WIDTH - MAP_WIDTH / 2,
+                1f,
+                (byte)r.Next(0, 200),
+                (byte)r.Next(0, 200),
+                (byte)r.Next(0, 200)
+            );
+
+            using (DarkRiftWriter newPlayerWriter = DarkRiftWriter.Create())
+            {
+                newPlayerWriter.Write(newPlayer.ID);
+                newPlayerWriter.Write(newPlayer.X);
+                newPlayerWriter.Write(newPlayer.Y);
+                newPlayerWriter.Write(newPlayer.Radius);
+                newPlayerWriter.Write(newPlayer.ColorR);
+                newPlayerWriter.Write(newPlayer.ColorG);
+                newPlayerWriter.Write(newPlayer.ColorB);
+
+                using (Message newPlayerMessage = Message.Create(Tags.SpawnPlayerTag, newPlayerWriter))
+                {
+                    foreach (IClient client in ClientManager.GetAllClients().Where(x => x != e.Client))
+                        client.SendMessage(newPlayerMessage, SendMode.Reliable);
+                }
+            }
+
+            players.Add(e.Client, newPlayer);
+
+            using (DarkRiftWriter playerWriter = DarkRiftWriter.Create())
+            {
+                foreach (Player player in players.Values)
+                {
+                    playerWriter.Write(player.ID);
+                    playerWriter.Write(player.X);
+                    playerWriter.Write(player.Y);
+                    playerWriter.Write(player.Radius);
+                    playerWriter.Write(player.ColorR);
+                    playerWriter.Write(player.ColorG);
+                    playerWriter.Write(player.ColorB);
+                }
+
+                using (Message playerMessage = Message.Create(Tags.SpawnPlayerTag, playerWriter))
+                    e.Client.SendMessage(playerMessage, SendMode.Reliable);
+            }
+        }
+    }
+
+    class Player
+    {
+        public ushort ID { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Radius { get; set; }
+        public byte ColorR { get; set; }
+        public byte ColorG { get; set; }
+        public byte ColorB { get; set; }
+
+        public Player(ushort ID, float x, float y, float radius, byte colorR, byte colorG, byte colorB)
+        {
+            this.ID = ID;
+            this.X = x;
+            this.Y = y;
+            this.Radius = radius;
+            this.ColorR = colorR;
+            this.ColorG = colorG;
+            this.ColorB = colorB;
+        }
     }
 }
